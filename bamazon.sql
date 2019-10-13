9e6e02b92170979fe6d7559463fd42c821b6980e
Zoe Longo <zoelongo@yahoo.com>
2019-10-12 22:30:31 -0600

    sql update

--- bamazon.sql
+++ bamazon.sql
@@ -4,23 +4,23 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-	item_id VARCHAR(100),
-    product_name VARCHAR(150),
-    department_name VARCHAR(150),
-    price DECIMAL(10, 8),
-    stock_quantity INT,
+	item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(150) NULL,
+    department_name VARCHAR(150) NULL,
+    price DECIMAL(10, 8) NULL,
+    stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
 
-USE bamazonDB;
+USE bamazonDB
 SELECT * FROM products;
 
-INSERT INTO products (item_id, product_name, deprtment_name, price, stock_quantity) VALUES
-("cereal1", "fruit loops", "groceries", 3.00, 10),
-("cereal2", "cheerios", "groceries", 3.25, 20),
-("cereal3", "fruity pebbles", "groceries", 3.50, 30),
-("cereal4", "apple jacks", "groceries", 3.75, 40),
-("cereal5", "honey crisp", "groceries", 4.00, 50),
-("cereal6", "lucky charms", "groceries", 4.25, 60),
-("cereal7", "frosted flakes", "groceries", 4.50, 70),
-("cereal8", "rasin bran", "groceries", 4.75, 80);
+INSERT INTO products (product_name, deprtment_name, price, stock_quantity) VALUES
+(`fruit loops`, `groceries`, 3.00, 10),
+(`cheerios`, `groceries`, 3.25, 20),
+(`fruity pebbles`, `groceries`, 3.50, 30),
+(`apple jacks`, `groceries`, 3.75, 40),
+(`honey crisp`, `groceries`, 4.00, 50),
+(`lucky charms`, `groceries`, 4.25, 60),
+(`frosted flakes`, `groceries`, 4.50, 70),
+(`rasin bran`, `groceries`, 4.75, 80);
