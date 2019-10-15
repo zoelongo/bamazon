999bf287d38868c620eb19acb59a496aefe39dd7
Zoe Longo <zoelongo@yahoo.com>
2019-10-14 20:38:37 -0600

    mysql and js update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-var inquirer = require(inquirer);
-var mysql = require(mysql);
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: `localhost`,
